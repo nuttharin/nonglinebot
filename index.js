fd93ea2441b1388819e47431a49dd18e193d5634
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2019-11-03 01:14:19 +0700

    nut

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
+const mysql = require('mysql')
 
 require('dotenv').config();
 
 const app = express();
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+})
 
 const config = {
     channelAccessToken: 'dpCgQdEGIbWJLxet0efql4UWyC2pXjupzEMpcqvk6uWlzKu7kreeSaay0Ghz5cwCWUp32LDHmzsUQGzOuyMY0tlS0c/NG51wHmBcficS99bYwmdtRjZbMWISp3WGdSEXLt+34p+hsHFEmVu4ag9W2AdB04t89/1O/w1cDnyilFU=',
@@ -31,7 +43,7 @@ function handleEvent(event) {
 function handleMessageEvent(event) {
     var msg = {
         type: 'text',
-        text: 'สวัสดีครัช'
+        text: 'สวัสดี'
     };
 
     var eventText = event.message.text.toLowerCase();
@@ -42,15 +54,15 @@ function handleMessageEvent(event) {
             'originalContentUrl': 'https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100',
             'previewImageUrl': 'https://images.performgroup.com/di/library/GOAL/a6/bb/fifa-18-ronaldo_lx3r88bpjpk91re36ukdgomrj.jpg?t=2027563652&w=620&h=430'
         }
-    } else if (eventText === 'location') {
+    } 
+    else if(eventText === 'test')
+    {
         msg = {
-            "type": "location",
-            "title": "my location",
-            "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-            "latitude": 35.65910807942215,
-            "longitude": 139.70372892916203
+            type: 'text',
+            text: 'Test line api'
         }
-    } else if (eventText === 'template button') {
+    }
+    else if (eventText === 'template button') {
         msg = {
             "type": "template",
             "altText": "this is a buttons template",
@@ -74,79 +86,8 @@ function handleMessageEvent(event) {
                 }]
             }
         }
-    } else if (eventText === 'template confirm') {
-        msg = {
-            "type": "template",
-            "altText": "this is a confirm template",
-            "template": {
-                "type": "confirm",
-                "text": "Are you sure?",
-                "actions": [{
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
-                }, {
-                    "type": "message",
-                    "label": "No",
-                    "text": "no"
-                }]
-            }
-        }
-    } else if (eventText === 'carousel') {
-        msg = {
-            "type": "template",
-            "altText": "this is a carousel template",
-            "template": {
-                "type": "carousel",
-                "columns": [
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=111"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=111"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/111"
-                            }
-                        ]
-                    },
-                    {
-                        "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
-                        "title": "this is menu",
-                        "text": "description",
-                        "actions": [
-                            {
-                                "type": "postback",
-                                "label": "Buy",
-                                "data": "action=buy&itemid=222"
-                            },
-                            {
-                                "type": "postback",
-                                "label": "Add to cart",
-                                "data": "action=add&itemid=222"
-                            },
-                            {
-                                "type": "uri",
-                                "label": "View detail",
-                                "uri": "http://example.com/page/222"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
+    } 
+  
 
     return client.replyMessage(event.replyToken, msg);
 }
